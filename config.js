24fe85940c55746629c71fe57cce0cbd97fe4c4a
ekdh0858 <94153997+ekdh0858@users.noreply.github.com>
2024-12-26 22:21:26 +0900

    프로필 변경

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
-    company: "weniv",
-    position: "CEO",
+    username: "taehun",
+    company: "none",
+    position: "devloper",
     img: "img/user/profile-licat.png",
   },
 ];
